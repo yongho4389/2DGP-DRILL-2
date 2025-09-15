a2027e4a71c2234cd5d463c20ffeb7240fb76033
yongho4389 <program0722@naver.com>
2025-09-15 14:06:15 +0900

    DRILL #2 제출 - 2022180037 임용호
    character_moving.py가 과제 수행 파일입니다.

--- character_moving.py
+++ character_moving.py
@@ -5,7 +5,7 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
-mode = False
+mode = True
 # 원 중심 좌표
 cx = 400
 cy = 320
@@ -47,14 +47,14 @@ while True:
     else:
         cycle = 1.5 # 2가 360도이므로 1.5는 270도
         for i in range(20):
-            radian = cycle * math.pi  # 270도를 라디안 각도로 바꾸어 저장
+            radian = cycle * math.pi  # 270도를 라디안 각도로 바꾸어 저장. 2파이가 360도를 나타내는 라디안이기 때문
             clear_canvas_now()
             grass.draw_now(400, 30)
             # cx, cy는 원의 중심 좌표, 220은 반지름
             x = cx + 220 * math.cos(radian)
             y = cy + 220 * math.sin(radian)
             character.draw_now(x, y)
-            cycle += 0.1
+            cycle -= 0.1
             delay(0.1)
         mode = True
 
